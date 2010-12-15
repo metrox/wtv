e00644c0a95a7f9beee6e04fce0b421f256408fb
metrox <dawiddyderski@gmail.com>
2010-12-15 09:19:04 +0100

    wtv

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require "rubygems"
+
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Fbwtv::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbwtv::Application.config.secret_token = '863639f14aa93e9f9537da31393abcb2e69cb39e931e4a2f1825106b43ed4d0376e5a489e4661f56037de912ef6eabd7a858cc636350769f9945d983b3ba969d'
